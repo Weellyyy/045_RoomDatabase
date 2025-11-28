411575e1c551cbd2e4b197da6968a96a52ad24aa
Weellyyy <wildanprtm2304@gmail.com>
2025-11-28 17:22:24 +0700

    membuat route untuk entry

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+import com.example.myroomsatu.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
