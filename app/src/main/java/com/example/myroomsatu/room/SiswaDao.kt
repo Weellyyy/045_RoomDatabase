643fa48478f5bfe85d63b474576796ca84077175
Weellyyy <wildanprtm2304@gmail.com>
2025-11-28 15:20:16 +0700

    membuat fungsi untuk mengambil data siswa berupalist

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -1,8 +1,11 @@
 package com.example.myroomsatu.room
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 
 interface SiswaDao {
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): List<Siswa>
 }
