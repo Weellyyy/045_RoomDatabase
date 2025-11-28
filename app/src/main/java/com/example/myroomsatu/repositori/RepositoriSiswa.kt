d8b5744e1ba3800602734cabe268b3d4ee0ea578
Weellyyy <wildanprtm2304@gmail.com>
2025-11-28 15:38:06 +0700

    membuat Fungsi untuk mengambil semua data siswa dan Fungsi untuk menambahkan data siswa baru.

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -1,5 +1,9 @@
 package com.example.myroomsatu.repositori
 
-interface RepositoriSiswa {
+import com.example.myroomsatu.room.Siswa
+import kotlinx.coroutines.flow.Flow
 
+interface RepositoriSiswa {
+    fun getAllSiswa(): Flow<List<Siswa>>
+    suspend fun insert(siswa: Siswa)
 }
