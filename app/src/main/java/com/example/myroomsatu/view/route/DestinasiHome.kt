1ad8d53d8f642eeaa4b095cab2197091732247ff
Weellyyy <wildanprtm2304@gmail.com>
2025-11-28 17:21:42 +0700

    membuat route untuk home

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.view.route.DestinasiNavigasi
+import com.example.myroomsatu.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.entry_siswa
+}
