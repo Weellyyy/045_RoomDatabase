c3a2a64ac1eee6300d30091598578419b11853bc
Weellyyy <wildanprtm2304@gmail.com>
2025-12-05 15:44:10 +0700

    membuat navigasi untuk detail

--- app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroomsatu.view.DetailSiswaScreen
 import com.example.myroomsatu.view.EntrySiswaScreen
 import com.example.myroomsatu.view.HomeScreen
+import com.example.myroomsatu.view.route.DestinasiDetailSiswa
+import com.example.myroomsatu.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.myroomsatu.view.route.DestinasiEntry
 import com.example.myroomsatu.view.route.DestinasiHome
 
@@ -38,6 +43,15 @@ fun HostNavigasi(
 
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+        composable (route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = { navController.navigateUp() }
+            )
         }
     }
 }
