3c891a0de448ff964fdd5d140137fbc6694b4e03
Weellyyy <wildanprtm2304@gmail.com>
2025-12-05 18:54:46 +0700

    membuat objetc untuk edit siswa

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiEditSiswa.kt
@@ -1,4 +1,10 @@
 package com.example.myroomsatu.view.route
 
-object DestinasiEditSiswa {
+import com.example.myroomsatu.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
